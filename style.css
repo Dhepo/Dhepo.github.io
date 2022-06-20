66c82f441355fef6b6e5b2f7bbfc8cf6e3eaade5
Dhepo <102391909+Dhepo@users.noreply.github.com>
2022-06-20 17:55:11 -0500

    Update style.css

---
+++ style.css
@@ -0,0 +1,20 @@
+h1 {
+  color:white;
+  background-color:lightGreen;
+  border:12px solid cyan;
+  padding: 5px;
+  border-radius: 15px;
+  text-align: center;
+}
+p{
+  color: blue;
+}
+p, h1, h2{
+  font-family: cursive;
+}
+img{
+  box-shadow: 10px 10px 15px grey;
+}
+p, img {
+  margin-left:50px;
+}
